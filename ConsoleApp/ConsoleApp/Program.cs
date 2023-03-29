91fac2491e89c969477f80a79b3ccf77535debd2
andrey-logunov-nyc <Andrey.Logunov@telusagcg.com>
2023-03-28 23:05:20 -0400

    Update Program.cs

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
